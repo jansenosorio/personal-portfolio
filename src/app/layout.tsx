8ac485ebd28a78ca201911e3f4b4c79597acf21b
jansenosorio <contato.devosorio@gmail.com>
2024-03-23 20:56:39 -0300

    feat: inserts fonts poppins and orbitron into project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Orbitron, Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["900"],
+  variable: "--font-orbitron",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${orbitron.variable} ${poppins.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   );
 }
